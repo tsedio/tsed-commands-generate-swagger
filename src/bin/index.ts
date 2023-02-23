6381513cc455452354a30b31d3f158adcdd9cf15
Romain Lenzotti <rom.lenzotti@gmail.com>
2023-02-23 17:50:43 +0100

    fix: used official tsed plugin to generate swagger spec

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import {CliCore} from "@tsed/cli-core";
 import {config} from "../config";
-import {GenerateSwaggerCmd} from "./commands/GenerateSwaggerCmd";
+import {GenerateSwaggerCmd} from "@tsed/cli-generate-swagger";
 
 CliCore.bootstrap({
   ...config,
